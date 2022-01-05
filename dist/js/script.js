be2a215e19d6b4343c9a9f431d315ca97b5c07bb
Jonathan van der Ceelen <j.vanderceelen@minddistrict.com>
2022-01-05 22:28:54 +0100

    Change js from vue to vanilla

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,130 +1,133 @@
-// import map from './map.js';
-// import monsters from './monsters.js';
+import map from './map.js';
+import monsters from './monsters.js';
 
 window.onload = function () {
-  console.log('hallo')
-  // var app = new Vue({
-  //   el: '#game',
+
+  const inputElement = document.getElementsByClassName('input-field')[0];
+  const outputElement = document.getElementsByClassName('output-field')[0];
+
+  inputElement.addEventListener('change', (event) => {
+    procesInput(event.target.value);
+  });
+
     // data: {
-    //   terminalInput: '',
+    //   textInput: '',
     //   feedback: '',
     //   currentCoords: [0,0],
     // },
-    // methods: {
-    //
-    //   procesTerminalInput: function() {
-    //     this.handleTerminalInput();
-    //     this.clearTerminalInput();
-    //   },
-    //
-    //   handleTerminalInput: function() {
-    //     switch(this.terminalInput) {
-    //       case '':
-    //         // do nothing
-    //         break;
-    //       case 'n':
-    //       case 's':
-    //       case 'e':
-    //       case 'w':
-    //         // move
-    //         this.move()
-    //         break;
-    //       case 'a':
-    //       case 'attack':
-    //         // attack
-    //         this.attack();
-    //         break;
-    //       case 'exits':
-    //         // attack
-    //         this.showExits();
-    //         break;
-    //       case 'location':
-    //         // attack
-    //         this.showCurrentLocation();
-    //         break;
-    //       case 'l':
-    //       case 'look':
-    //         // attack
-    //         this.describeLocation();
-    //         break;
-    //       default:
-    //         // code block
-    //         return;
-    //     }
-    //   },
-    //
-    //   clearTerminalInput: function() {
-    //     this.terminalInput = '';
-    //   },
-    //
-    //   move: function() {
-    //
-    //     if (!this.hasExit()) {
-    //       this.setFeedback('No exit in that direction.');
-    //       return;
-    //     }
-    //
-    //     var direction = '';
-    //
-    //     switch(this.terminalInput) {
-    //       case 'n':
-    //         // direction = 'north';
-    //         this.currentCoords[0] += 1;
-    //         break;
-    //       case 's':
-    //         // direction = 'south';
-    //         this.currentCoords[0] -= 1;
-    //         break;
-    //       case 'e':
-    //         // direction = 'east';
-    //         this.currentCoords[1] += 1;
-    //         break;
-    //       case 'w':
-    //         // direction = 'west';
-    //         this.currentCoords[1] -= 1;
-    //         break;
-    //     }
-    //
-    //     this.checkLocation();
-    //   },
-    //
-    //   hasExit: function() {
-    //     let exits = map[this.currentCoords.toString()].exits;
-    //
-    //     if (exits.includes(this.terminalInput)) {
-    //       return true;
-    //     }
-    //   },
-    //
-    //   showExits: function() {
-    //     this.setFeedback(map[this.currentCoords.toString()].exits);
-    //   },
-    //
-    //   showCurrentLocation: function() {
-    //     this.setFeedback(map[this.currentCoords.toString()].name);
-    //   },
-    //
-    //   checkLocation: function() {
-    //
-    //     for (var coords in map) {
-    //
-    //       if (coords == this.currentCoords.toString()) {
-    //         this.setFeedback('You arrive at ' + map[coords].name);
-    //       }
-    //     }
-    //   },
-    //
-    //   describeLocation: function() {
-    //     this.setFeedback(map[this.currentCoords].description);
-    //   },
-    //
-    //   setFeedback: function(feedback) {
-    //     this.feedback = '<span class="feedback">' + feedback + '</span>\n';
-    //   },
-    //
-    //   attack: function() {
-    //     this.setFeedback('yaaaaaaaaaaaaaaaaaar');
-    //   },
-    // }
-  // })
+
+  function procesInput(textInput) {
+    handleInput(textInput);
+    clearInputField();
+  }
+
+  function handleInput(textInput) {
+    switch(textInput) {
+      case '':
+        // do nothing
+        break;
+      case 'n':
+      case 's':
+      case 'e':
+      case 'w':
+        // move
+        move(textInput)
+        break;
+      case 'a':
+      case 'attack':
+        // attack
+        attack();
+        break;
+      case 'exits':
+        // attack
+        showExits();
+        break;
+      case 'location':
+        // attack
+        showCurrentLocation();
+        break;
+      case 'l':
+      case 'look':
+        // attack
+        describeLocation();
+        break;
+      default:
+        // code block
+        return;
+    }
+  }
+
+  function clearInputField() {
+    inputElement.value = '';
+  }
+
+  function move(textInput) {
+
+    if (!hasExit()) {
+      setFeedback('No exit in that direction.');
+      return;
+    }
+
+    var direction = '';
+
+    switch(textInput) {
+      case 'n':
+        // direction = 'north';
+        currentCoords[0] += 1;
+        break;
+      case 's':
+        // direction = 'south';
+        currentCoords[0] -= 1;
+        break;
+      case 'e':
+        // direction = 'east';
+        currentCoords[1] += 1;
+        break;
+      case 'w':
+        // direction = 'west';
+        currentCoords[1] -= 1;
+        break;
+    }
+
+    checkLocation();
+  }
+
+  function hasExit() {
+    let exits = map[currentCoords.toString()].exits;
+
+    if (exits.includes(textInput)) {
+      return true;
+    }
+  }
+
+  function showExits() {
+    setFeedback(map[currentCoords.toString()].exits);
+  }
+
+  function showCurrentLocation() {
+    setFeedback(map[currentCoords.toString()].name);
+  }
+
+  function checkLocation() {
+
+    for (var coords in map) {
+
+      if (coords == currentCoords.toString()) {
+        setFeedback('You arrive at ' + map[coords].name);
+      }
+    }
+  }
+
+  function describeLocation() {
+    setFeedback(map[currentCoords].description);
+  }
+
+  function setFeedback(feedback) {
+    feedback = '<span class="feedback">' + feedback + '</span>\n';
+    outputElement.innerHTML += feedback;
+  }
+
+  function attack() {
+    setFeedback('yaaaaaaaaaaaaaaaaaar');
+  }
 }
